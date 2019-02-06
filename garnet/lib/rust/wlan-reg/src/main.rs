d778783a134dd110381124b46b6634d691b717dd
Jiwoong Lee <porce@google.com>
2019-02-05 17:09:04 -0800

    [wlan][reg] Command line executable binary to test
    Change-Id: Ibc54bd0d9b30a0cf2787af1263b32271e0d4daac

--- garnet/lib/rust/wlan-reg/src/main.rs
+++ garnet/lib/rust/wlan-reg/src/main.rs
@@ -1,3 +1,29 @@
+#![allow(unused)]
+use log::{error, info};
+
+use failure::Error;
+
+#[macro_use]
+
+mod loader;
+mod country;
+mod utils;
+
 fn main() {
-    println!("Hello, world!");
+    let jurisdiction = country::get_jurisdiction();
+    let filepath = loader::get_operating_class_filename(&jurisdiction);
+
+    let toml = match loader::load_operating_class_toml(&filepath.to_string()) {
+        Err(e) => {
+            error!("{}", e);
+            return;
+        }
+        Ok(t) => t,
+    };
+
+    println!("\nFor jurisdiction: {}", jurisdiction);
+    println!("  File: {}", filepath);
+    println!("  Parse result:\n{:?}\n", toml);
+    println!("   From file contents:");
+    utils::dump_file(&filepath);
 }
