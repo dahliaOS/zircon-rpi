f4e8061e212ae06a6d04f89e3c2061f5cb5ca62a
Jiwoong Lee <porce@google.com>
2019-02-08 16:58:51 -0800

    [wlan][reg] Test code for regulation
    Change-Id: Ic0e84b5b7a81e056f3c3bfa40c3640e3f91ae1f3

--- garnet/lib/rust/wlan-reg/src/main.rs
+++ garnet/lib/rust/wlan-reg/src/main.rs
@@ -6,7 +6,7 @@ use failure::Error;
 use log::{error, info};
 use wlan_reg::*;
 
-fn main() {
+fn play_operating_class() {
     let jurisdiction = country::get_jurisdiction();
     let filepath = loader::get_operating_class_filename(&jurisdiction);
 
@@ -20,12 +20,32 @@ fn main() {
 
     println!("\nFor jurisdiction: {}", jurisdiction);
     println!("  File: {}", filepath);
-    //    println!("  Parse result:\n{:?}\n", toml);
-    //    println!("   From file contents:");
-    //    utils::dump_file(&filepath);
+    println!("  Parse result:\n{:?}\n", toml);
+    println!("   From file contents:");
+    utils::dump_file(&filepath);
 
     let channel_groups =
         channel::build_channel_groups(&toml, &country::get_active_operating_classes());
 
     println!("{}", channel_groups);
+}
+
+fn play_regulation() {
+    let jurisdiction = country::get_jurisdiction();
+    let filepath = loader::get_regulation_filename(&jurisdiction);
+
+    let toml = match loader::load_regulation_toml(&filepath.to_string()) {
+        Err(e) => {
+            error!("{}", e);
+            println!("{}", e);
+            return;
+        }
+        Ok(t) => t,
+    };
+
+    println!("{:#?}", toml);
+}
+
+fn main() {
+    play_regulation();
 }
