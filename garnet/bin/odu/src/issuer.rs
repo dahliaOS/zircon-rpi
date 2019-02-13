9576d014a5c517da429ab0fbfea326a343ceb646
Vikram Auradkar <auradkar@google.com>
2019-02-13 15:23:27 -0800

    Odu - IO benchmarking utility
    This is first cut to keep review rolling. The patch allows me to
    incorporate feedback from review as I keep working on the tool.
    There are plenty of TODOs that might serve as a hint to reviewers
    what I intend to do in the future, There are codes that look noop
    which I have retained because it makes testing easier.
    The current version parses only one argument (a file name) and
    runs load for few seconds or a minute at max.
    
    When run - output looks like this.
    $ odu /tmp/f
    ===== Aggregate Stats =====
    Runtime:50.15060829s IOs:303 write_size(MB):62.40
    IOPS:4.79 LAT(us):208579.46 BW(MB/s):0.98
    
    If I print thread specific stats and stage stats the output would look
    like
    
    $ odu /tmp/e
    ===== For generator-0 =====
    Generate TimeInStage:344791630 IOPS:292.93 LAT(us):3413.78
    BW(MB/s):58.01
    Issue TimeInStage:24016252450 IOPS:4.21 LAT(us):237784.68 BW(MB/s):0.83
    Verify TimeInStage:6253520 IOPS:16150.90 LAT(us):61.92 BW(MB/s):3198.20
    Runtime:49.78938866s IOs:101 write_size(MB):20.80
    IOPS:4.21 LAT(us):237784.68 BW(MB/s):0.83
    ===== For generator-1 =====
    Generate TimeInStage:494067320 IOPS:204.43 LAT(us):4891.76
    BW(MB/s):40.48
    Issue TimeInStage:22197400730 IOPS:4.55 LAT(us):219776.24 BW(MB/s):0.90
    Verify TimeInStage:1960320 IOPS:51522.20 LAT(us):19.41 BW(MB/s):10202.42
    Runtime:52.46426601s IOs:101 write_size(MB):20.80
    IOPS:4.55 LAT(us):219776.24 BW(MB/s):0.90
    ===== For generator-2 =====
    Generate TimeInStage:634360370 IOPS:159.22 LAT(us):6280.80
    BW(MB/s):31.53
    Issue TimeInStage:24582622520 IOPS:4.11 LAT(us):243392.30 BW(MB/s):0.81
    Verify TimeInStage:21145570 IOPS:4776.41 LAT(us):209.36 BW(MB/s):945.82
    Runtime:48.96960358s IOs:101 write_size(MB):20.80
    IOPS:4.11 LAT(us):243392.30 BW(MB/s):0.81
    ===== Aggregate Stats =====
    Generate TimeInStage:1473219320 IOPS:205.67 LAT(us):4862.11
    BW(MB/s):42.08
    Issue TimeInStage:70796275700 IOPS:4.28 LAT(us):233651.07 BW(MB/s):0.88
    Verify TimeInStage:29359410 IOPS:10320.37 LAT(us):96.90 BW(MB/s):2111.76
    Runtime:52.47356284s IOs:303 write_size(MB):62.40
    IOPS:4.28 LAT(us):233651.07 BW(MB/s):0.88
    
    Utility saves the arguments to generator in a file called
    "./output.confing" whose content may look like
    [{"magic_number":5720826294568186926,"process_id":3,"name":"generator-0",
    "generator_unique_id":0,"block_size":4096,"max_io_size":1048576,"align":
    true,"seed":0,"target_name":"e","target_range":{"start":0,"end":6990506},
    "target_type":"target_file","issuer_queue_depth":40,"max_io_count":100,
    "sequential":true}...]
    
    Tested: fx run-test odu_tests -- --nocapture
    
    Change-Id: I219379093dfef14a0979678ce2e34a76fc5b9bbf

---
+++ garnet/bin/odu/src/issuer.rs
@@ -0,0 +1,152 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#![deny(warnings)]
+
+use {
+    crate::generator::ActiveCommands,
+    crate::io_packet::{InternalCommand, IoPacketType},
+    crate::operations::PipelineStages,
+    failure::Error,
+    log::{debug, error, warn},
+    std::{
+        process,
+        sync::mpsc::{Receiver, Sender},
+    },
+};
+
+pub struct IssuerArgs {
+    /// Human friendly name for this thread.
+    name: String,
+
+    /// Unique identifier for each generator.
+    issuer_unique_id: u64,
+
+    // Issuer's stage in lifetime of an IO.
+    stage: PipelineStages,
+
+    // Channel used to receive commands from generator
+    from_generator: Receiver<IoPacketType>,
+
+    // Channel used to send commands to verifier
+    to_verifier: Sender<IoPacketType>,
+
+    // Channel used to receive commands from verifier
+    from_verifier: Receiver<IoPacketType>,
+
+    // Way for generator and verifier to notify the issuer that there are one or
+    // more commands in the queue.
+    active_commands: ActiveCommands,
+}
+
+impl IssuerArgs {
+    pub fn new(
+        base_name: String,
+        issuer_unique_id: u64,
+        from_generator: Receiver<IoPacketType>,
+        to_verifier: Sender<IoPacketType>,
+        from_verifier: Receiver<IoPacketType>,
+        active_commands: ActiveCommands,
+    ) -> IssuerArgs {
+        IssuerArgs {
+            name: String::from(format!("{}-{}", base_name, issuer_unique_id)),
+            issuer_unique_id: issuer_unique_id,
+            from_generator: from_generator,
+            to_verifier: to_verifier,
+            from_verifier: from_verifier,
+            stage: PipelineStages::Issue,
+            active_commands: active_commands,
+        }
+    }
+}
+
+pub fn run_issuer(mut args: IssuerArgs) -> Result<(), Error> {
+    // Even on happy path, either generator or verifier can be the first to
+    // close the channel. These two variables keep track of whether the channel
+    // was closed or not.
+    let mut scan_generator = true;
+    let mut scan_verifier = true;
+
+    // True if the current command is from verifier.
+    let mut verifying_cmd;
+
+    let mut cmd_or_err;
+    let mut cmd;
+
+    // This thread/loop is not done till we hear explicitly from generator and
+    // from verifier that they both are done. We keep track of who is done.
+    while scan_generator || scan_verifier {
+        // true if the current command is received from verifier
+        let mut verifying_cmd = false;
+
+        // May block
+        args.active_commands.decrement();
+
+        // There is at least one command in the queues. We don't know in which
+        // yet.
+        // Lets give priority to io packets from verifier.
+        cmd_or_err = args.from_verifier.try_recv();
+        if !cmd_or_err.is_err() {
+            verifying_cmd = true;
+        }
+
+        // if we found a command on verifier channel, don't look for command on
+        // generator channel
+        if !verifying_cmd {
+            cmd_or_err = args.from_generator.try_recv();
+        }
+
+        cmd = cmd_or_err.unwrap();
+
+        debug!(
+            "from issuer: {} id: {} io_seq: {} op: {:?} verifying_cmd: {}",
+            args.name,
+            args.issuer_unique_id,
+            cmd.sequence_number(),
+            cmd.operation_type(),
+            verifying_cmd,
+        );
+
+        cmd.timestamp_stage_start(&args.stage);
+        cmd.do_io();
+        if !cmd.is_complete() {
+            error!("Asynchronous commands not implemented yet.");
+            process::abort();
+        }
+
+        // Mark done timestamps.
+        cmd.timestamp_stage_end(&args.stage);
+
+        // Cloning the command
+        let internal_command = cmd.abort_or_exit();
+
+        // Check if this was an internal command and if so take appropriate
+        // action.
+        match internal_command {
+            InternalCommand::Exit => {
+                if verifying_cmd {
+                    scan_verifier = false;
+                    // if this internal command is coming from verifier,
+                    // skip sending it to verifier.
+                    continue;
+                } else {
+                    scan_generator = false;
+                }
+                debug!("{} - clean exit", args.name);
+            }
+            InternalCommand::Abort => {
+                warn!("{} - aborted", args.name);
+                break;
+            }
+            InternalCommand::None => {}
+        }
+
+        if args.to_verifier.send(cmd).is_err() {
+            error!("error sending command from issuer");
+            process::abort();
+        }
+    }
+
+    Ok(())
+}
---
+++ garnet/bin/odu/src/main.rs
@@ -0,0 +1,159 @@
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#![deny(warnings)]
+
+mod file_target;
+mod generator;
+mod io_packet;
+mod issuer;
+mod log;
+mod operations;
+mod sequential_io_generator;
+mod verifier;
+
+extern crate serde;
+
+use {
+    crate::generator::{run_generator, GeneratorArgs},
+    crate::log::{log_init, Stats},
+    ::log::{debug, log_enabled, Level::Debug},
+    failure::Error,
+    std::{
+        env,
+        fs::{metadata, File, OpenOptions},
+        io::prelude::*,
+        process,
+        sync::{Arc, Mutex},
+        thread::spawn,
+        time::Instant,
+    },
+};
+
+// Magic number that gets written in block header
+static MAGIC_NUMBER: u64 = 0x4f6475346573742e;
+
+fn create_target(target_name: &String, target_length: u64) {
+    let metadata = metadata(&target_name);
+
+    match metadata {
+        Ok(stats) => {
+            assert!(!stats.permissions().readonly());
+            assert!(stats.len() >= target_length);
+            return;
+        }
+        _ => {}
+    }
+    // TODO(auradkar): File should not be created here. It is generator/target's
+    // knowledge/job/responsibility.
+    let f = File::create(&target_name).unwrap();
+    f.set_len(target_length).unwrap();
+}
+
+fn parse_target_name() -> String {
+    let args: Vec<String> = env::args().collect();
+    args[1].to_string()
+}
+
+fn output_config(generator_args_vec: &Vec<GeneratorArgs>, output_config_file: &String) {
+    let serialized = serde_json::to_string(&generator_args_vec).unwrap();
+    let mut file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(&output_config_file)
+        .unwrap();
+    file.write_all(serialized.as_bytes()).unwrap();
+
+    debug!("{}", serialized);
+    file.sync_data().unwrap();
+}
+
+fn main() -> Result<(), Error> {
+    // These are a bunch of inputs that each generator thread receives. These
+    // should be received as input to the app.
+    // TODO(auradkar): Implement args parsing and validation logic.
+    let issuer_queue_depth: u64 = 40;
+    let block_size: u64 = 4096;
+    let max_io_size: u64 = 8 * 1024;
+    let align: bool = true;
+    let max_io_count: u64 = 1000;
+    let target_length: u64 = 20 * 1024 * 1024;
+    let thread_count: u64 = 3;
+    let target_type_file: &str = "target_file";
+    let sequential: bool = true;
+    let output_config_file: &str = "/tmp/output.config";
+
+    let start_instant: Instant = Instant::now();
+    log_init().unwrap();
+
+    let mut thread_handles = vec![];
+    let mut generator_args_vec = vec![];
+
+    let file_name = parse_target_name();
+    create_target(&file_name, target_length);
+
+    let metadata = metadata(&file_name)?;
+    // lazy_static::initialize(&start_instant);
+
+    let mut offset_start = 0 as u64;
+    let range_size = (metadata.len() / thread_count as u64) as u64;
+
+    // To keep contention among threads low, each generator owns/updates their
+    // stats. The "main" thread holds a
+    // reference to these stats so that, when ready, it can print the
+    // progress/stats from time to time.
+    let mut stats_array: Vec<Arc<Mutex<Stats>>> = Vec::with_capacity(thread_count as usize);
+
+    for i in 0..thread_count {
+        let args = GeneratorArgs::new(
+            MAGIC_NUMBER,
+            process::id() as u64,
+            i, // generator id
+            block_size,
+            max_io_size,
+            align,
+            i, // seed
+            file_name.to_string(),
+            offset_start..(offset_start + range_size),
+            target_type_file.to_string(),
+            issuer_queue_depth,
+            max_io_count,
+            sequential,
+        );
+        generator_args_vec.push(args.clone());
+
+        let stats = {
+            let mut stats = Stats::new();
+            stats.start_clock();
+            Arc::new(Mutex::new(stats))
+        };
+        stats_array.push(stats.clone());
+        thread_handles.push(spawn(move || run_generator(args, start_instant.clone(), stats)));
+        offset_start += range_size;
+    }
+
+    output_config(&generator_args_vec, &output_config_file.to_string());
+    for handle in thread_handles {
+        handle.join().unwrap()?;
+    }
+
+    let mut aggregate_stats = Stats::new();
+    let mut i = 0;
+
+    // How the summary stats. For long running IO load we should print the stats
+    // from time to time to show how IO are going. A TODO(auradkar).
+    for stat in stats_array {
+        let stat = stat.lock().unwrap();
+        aggregate_stats.aggregate_summary(&stat);
+        if log_enabled!(Debug) {
+            debug!("===== For generator-{} =====", i);
+            stat.display_summary();
+            i += 1;
+        }
+    }
+    println!("===== Aggregate Stats =====");
+    aggregate_stats.display_summary();
+
+    Ok(())
+}
