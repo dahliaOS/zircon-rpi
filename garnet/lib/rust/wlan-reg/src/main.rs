d82a970c606cb814b0d1e0983febe2f79643ccc5
Jiwoong Lee <porce@google.com>
2019-02-13 17:20:32 -0800

    [wlan][reg] Enrich the test code
    New Output format:
    
    Fuchsia WLAN Countries and Regulation Test
    
    Jurisdiction        : US
    TOML for OperClass  : [Valid]    ./data/operating_class_US.toml
    TOML for Regulation : [Valid]    ./data/regulation_US.toml
    TOML for DeviceCap  : [Valid]    ./data/device_capability_XYZ.toml
    
    [Legit Channel Group]
    2 GHz               : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
    5 GHz               : [36, 40, 44, 48, 52, 56, 60, 64, 100, 104, 108, 112, 116, 120, 124, 128, 132, 136, 140, 144, 149, 153, 157, 161, 165]
    DFS                 : [52, 56, 60, 64, 100, 104, 108, 112, 116, 120, 124, 128, 132, 136, 140, 144]
    CBW40 Above         : [1, 2, 3, 4, 5, 6, 7, 36, 44, 52, 60, 100, 108, 116, 124, 132, 140, 149, 157]
    CBW40 Below         : [40, 48, 56, 64, 104, 112, 120, 128, 136, 144, 153, 161]
    CBW80 Center        : [42, 58, 106, 122, 138, 155]
    CBW160 Center       : [50, 114]
    All                 : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 36, 40, 42, 44, 48, 50, 52, 56, 58, 60, 64, 100, 104, 106, 108, 112, 114, 116, 120, 122, 124, 128, 132, 136, 138, 140, 144, 149, 153, 155, 157, 161, 165]
    
    [Operation Channel Group]
    2 GHz               : [1, 6, 11]
    5 GHz               : [36, 40, 44, 48, 52, 56, 60, 64, 104, 108, 112, 116, 120, 124, 128, 132, 136, 140, 149, 153, 157, 161, 165]
    DFS                 : [52, 56, 60, 64, 104, 108, 112, 116, 120, 124, 128, 132, 136, 140]
    CBW40 Above         : [36, 44, 52, 60, 108, 116, 124, 132, 149, 157]
    CBW40 Below         : [40, 48, 56, 64, 112, 120, 128, 136, 153, 161]
    CBW80 Center        : [42, 58, 122, 155]
    CBW160 Center       : [50]
    All                 : [1, 6, 11, 36, 40, 42, 44, 48, 50, 52, 56, 58, 60, 64, 104, 108, 112, 116, 120, 122, 124, 128, 132, 136, 140, 149, 153, 155, 157, 161, 165]
    
    [Sample channel configurations]
    Device capabilities  : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 36, 40, 44, 48, 52, 56, 60, 64, 100, 104, 108, 112, 116, 120, 124, 128, 132, 136, 140, 144, 149, 153, 157, 161, 165]
    Planned Non-operate  : [2, 3, 4, 5, 7, 8, 9, 10, 144]
    Blocked              : [100]
    
    [Power Budget]
    Chan    1 :  30 dBm
    Chan    2 :  30 dBm
    Chan    3 :  30 dBm
    Chan    4 :  30 dBm
    Chan    5 :  30 dBm
    Chan    6 :  30 dBm
    Chan    7 :  30 dBm
    Chan    8 :  30 dBm
    Chan    9 :  30 dBm
    Chan   10 :  30 dBm
    Chan   11 :  30 dBm
    Chan   36 :  24 dBm
    Chan   40 :  24 dBm
    Chan   42 :  24 dBm
    Chan   44 :  24 dBm
    Chan   48 :  24 dBm
    Chan   50 :  24 dBm
    Chan   52 :  24 dBm
    Chan   56 :  24 dBm
    Chan   58 :  24 dBm
    Chan   60 :  24 dBm
    Chan   64 :  24 dBm
    Chan  100 :  24 dBm
    Chan  104 :  24 dBm
    Chan  106 :  24 dBm
    Chan  108 :  24 dBm
    Chan  112 :  24 dBm
    Chan  114 :  24 dBm
    Chan  116 :  24 dBm
    Chan  120 :  24 dBm
    Chan  122 :  24 dBm
    Chan  124 :  24 dBm
    Chan  128 :  24 dBm
    Chan  132 :  24 dBm
    Chan  136 :  24 dBm
    Chan  138 :  24 dBm
    Chan  140 :  24 dBm
    Chan  144 :  30 dBm
    Chan  149 :  30 dBm
    Chan  153 :  30 dBm
    Chan  155 :  30 dBm
    Chan  157 :  30 dBm
    Chan  161 :  30 dBm
    Chan  165 :  30 dBm
    
    Change-Id: Idbd73c5ebc0bbd92434b002e83ada3053ed9ff15

--- garnet/lib/rust/wlan-reg/src/main.rs
+++ garnet/lib/rust/wlan-reg/src/main.rs
@@ -4,58 +4,48 @@ extern crate wlan_reg;
 
 use failure::Error;
 use log::{error, info};
+use std::collections::HashMap;
 use wlan_reg::*;
 
-fn play_operating_class() {
-    let jurisdiction = country::get_jurisdiction();
-    let filepath = operclass::get_filepath(&jurisdiction);
+fn show_operclass() {
+    let juris = country::get_jurisdiction();
+    let filepath = operclass::get_filepath(&juris);
+    let is_valid = operclass::load_toml(&filepath.to_string()).is_ok();
 
-    let toml = match operclass::load_toml(&filepath.to_string()) {
-        Err(e) => {
-            error!("{}", e);
-            return;
-        }
-        Ok(t) => t,
-    };
-
-    println!("\nFor jurisdiction: {}", jurisdiction);
-    println!("  File: {}", filepath);
-    //    println!("  Parse result:\n{:?}\n", toml);
-    //    println!("   From file contents:");
-    //    utils::dump_file(&filepath);
-
-    //    let channel_groups =
-    //        channel::build_legitimate_group(&toml, &device_cap::get_active_operating_classes());
-    //
-    //    println!("{}", channel_groups);
+    println!(
+        "{:20}: {:10} {}",
+        "TOML for OperClass",
+        if is_valid { "[Valid]" } else { "[Invalid]" },
+        filepath
+    );
 }
 
-fn play_regulation() {
-    let jurisdiction = country::get_jurisdiction();
-    let filepath = regulation::get_filepath(&jurisdiction);
+fn show_regulation() {
+    let juris = country::get_jurisdiction();
+    let filepath = regulation::get_filepath(&juris);
+    let is_valid = regulation::load_toml(&filepath.to_string()).is_ok();
 
-    let toml = match regulation::load_toml(&filepath.to_string()) {
-        Err(e) => {
-            error!("{}", e);
-            println!("{}", e);
-            return;
-        }
-        Ok(t) => t,
-    };
-
-    // println!("{:#?}", toml);
-    let budget_by_range = power::build_power_budget_by_range(&toml, "client");
-    match budget_by_range {
-        Err(e) => error!("{:?}", e),
-        Ok(r) => {
-            for e in r.iter() {
-                println!("{:?}", e)
-            }
-        }
-    }
+    println!(
+        "{:20}: {:10} {}",
+        "TOML for Regulation",
+        if is_valid { "[Valid]" } else { "[Invalid]" },
+        filepath
+    );
 }
 
-fn main() {
+fn show_device_cap() {
+    let filepath = device_cap::get_filepath();
+    let is_valid = device_cap::load_toml(&filepath.to_string()).is_ok();
+
+    println!(
+        "{:20}: {:10} {}",
+        "TOML for DeviceCap",
+        if is_valid { "[Valid]" } else { "[Invalid]" },
+        filepath
+    );
+}
+
+fn show_legit_chan_group() {
     let legit_chan_group = channel::get_legitimate_group();
     match legit_chan_group {
         Err(e) => {
@@ -63,14 +53,12 @@ fn main() {
             println!("{:?}", e);
         }
         Ok(l) => {
-            println!("\nLegit Channel Group\n{}", l);
+            println!("\n[Legit Channel Group]\n{}", l);
         }
     }
+}
 
-    println!("Device capable channels   {:?}", device_cap::get_channels());
-    println!("Planned non-oper channels {:?}", channel::get_planned_non_operation_chanidx_list());
-    println!("Blocked channels          {:?}", channel::get_blocked_chanidx_list());
-
+fn show_oper_chan_group() {
     let oper_chan_group = channel::get_operation_group();
     match oper_chan_group {
         Err(e) => {
@@ -78,18 +66,59 @@ fn main() {
             println!("{:?}", e);
         }
         Ok(l) => {
-            println!("\nOperation Channel Group\n{}", l);
+            println!("\n[Operation Channel Group]\n{}", l);
         }
     }
+}
 
-    let budget = power::get_power_budget_for_client();
-    match budget {
+fn show_sample_chan_config() {
+    let device_chans: Vec<u8> = match device_cap::get_channels() {
+        Err(e) => vec![],
+        Ok(v) => v,
+    };
+    println!("\n[Sample channel configurations]");
+    println!("{:20} : {:?}", "Device capabilities", device_chans);
+    println!("{:20} : {:?}", "Planned Non-operate", channel::get_planned_non_operation_chanidx_list());
+    println!("{:20} : {:?}", "Blocked", channel::get_blocked_chanidx_list());
+}
+
+fn show_power_budget() {
+    let budget = match power::get_power_budget_for_client() {
         Err(e) => {
-            error!("{:?}", e);
-            println!("{:?}", e)
+            println!("\n[Power Budget (Chan index: dBm)]\n{}", "[Invalid]");
+            return;
         }
-        Ok(b) => {
-            println!("Power Budget By Channel Index\n{:?}", b);
-        }
+        Ok(b) => b,
+    };
+
+    let mut budget_vec: Vec<(u8, i8)> = vec![];
+
+    for (k, v) in budget.iter() {
+        budget_vec.push((*k, *v));
     }
+    budget_vec.sort();
+
+    println!("\n[Power Budget]");
+    for (k, v) in budget_vec.iter() {
+        println!("Chan {:>4} : {:>3} dBm", k, v);
+    }
+}
+
+fn main() {
+    println!("\nFuchsia WLAN Countries and Regulation Test\n");
+
+    let juris = country::get_jurisdiction();
+    println!("{:20}: {}", "Jurisdiction", juris);
+
+    show_operclass();
+    show_regulation();
+    show_device_cap();
+
+    show_legit_chan_group();
+    show_oper_chan_group();
+
+    show_sample_chan_config();
+
+    show_power_budget();
+    println!("");
 }
