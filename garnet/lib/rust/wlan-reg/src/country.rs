9fb4071c83b97886b91343794996bc559c6cede7
Jiwoong Lee <porce@google.com>
2019-03-03 19:36:11 +0000

    [wlan][reg] Validate ISO alpha2 country codes
    Change-Id: If474a0a02e26ab3425a7030c87f3b77a1ae759aa

--- garnet/lib/rust/wlan-reg/src/country.rs
+++ garnet/lib/rust/wlan-reg/src/country.rs
@@ -1,7 +1,7 @@
 use super::utils;
 /// Returns the jurisdiction of operation.
 /// Never fails to return. The fallback is "GLOBAL".
-use failure::Error;
+use failure::{bail, Error};
 
 #[allow(dead_code)]
 #[derive(Deserialize)]
@@ -18,5 +18,24 @@ pub fn load_iso_alpha2() -> Result<Vec<String>, Error> {
     const FILENAME: &str = "./data/iso_alpha2.toml";
     let contents = utils::load_file(FILENAME)?;
     let iso_alpha2: IsoAlpha2 = toml::from_str(contents.as_str())?;
+
+    validate_iso_alpha2(&iso_alpha2)?;
+
     Ok(iso_alpha2.country_codes)
+}
+
+fn validate_iso_alpha2(iso_alpha2: &IsoAlpha2) -> Result<(), Error> {
+    for c in &iso_alpha2.country_codes {
+        // Two bytes ASCII string in upper case.
+        if c.len() != 2 {
+            bail!("Country code '{}' is not 2 bytes long", c);
+        }
+
+        let is_ascii = c.to_ascii_uppercase() == *c;
+        if !is_ascii {
+            bail!("Country code '{}' is not ASCII uppercase", c);
+        }
+    }
+
+    Ok(())
 }
