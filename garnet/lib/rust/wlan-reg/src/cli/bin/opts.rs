791b0f913edcc9cd39950efe29b2d77a1c75f905
Jiwoong Lee <porce@google.com>
2019-03-20 13:57:56 -0700

    [wlan][reg] Fix help messages
    Change-Id: I2a6aae0eb9de99e2b741f9ce40b884f431f0f2ff

--- garnet/lib/rust/wlan-reg/src/cli/bin/opts.rs
+++ garnet/lib/rust/wlan-reg/src/cli/bin/opts.rs
@@ -10,33 +10,39 @@ pub enum Opt {
 
 #[derive(Debug, StructOpt, Clone)]
 pub enum ShowCommand {
-    #[structopt(name = "operclass")]
+    #[structopt(
+        name = "operclass",
+        about = "Shows operating classes defined in the specified jurisdiction."
+    )]
     OperClass {
         #[structopt(raw(required = "true"))]
         jurisdiction: String,
     },
 
-    #[structopt(name = "jurisdictions-all", help = "show all jurisdictions supported")]
+    #[structopt(name = "jurisdictions-all", about = "Shows all jurisdictions supported")]
     AllJurisdictions,
 
-    #[structopt(name = "regulation")]
+    #[structopt(
+        name = "regulation",
+        about = "Shows regulations defined in the specified jurisdiction."
+    )]
     Regulation {
         #[structopt(raw(required = "true"))]
         jurisdiction: String,
     },
 
-    #[structopt(name = "device-meta")]
+    #[structopt(name = "device-meta", about = "Shows device meta capabilities")]
     DeviceMeta,
 
-    #[structopt(name = "jurisdiction-active", help = "show the active jurisdiction")]
+    #[structopt(name = "jurisdiction-active", about = "Shows the active jurisdiction")]
     ActiveJurisdiction,
 
-    #[structopt(name = "power-budget", help = "show power budget in the active jurisdiction")]
+    #[structopt(name = "power-budget", about = "Shows power budget in the active jurisdiction")]
     PowerBudget,
 
     #[structopt(
         name = "channel-groups",
-        help = "show channel groups for the underlying device in the active jurisdiction"
+        about = "Shows channel groups for the underlying device in the active jurisdiction"
     )]
     ChannelGroups,
 }
