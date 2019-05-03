b6bb57186b34c3429cd6731b30f29f0b84f08dca
Xo Wang <xow@google.com>
2019-05-02 19:41:58 -0700

    [bt][gap-bredr] Drive pairing state transitions using event handlers
    Add skeleton event handlers to PairingState for the HCI events it will
    respond to. The handlers have no side effects but signaling the wrong
    HCI event on a state causes an externally-observable error.
    
    Test that the state transitions occur as expected and errors out when
    events are not received in the order expected.
    
    BT-374
    
    Test: In bt-host-unittests
    GAP_PairingStateTest/HandlesEvent.*
    
    Change-Id: I2125ef74a8d065a904c95486af9ea4ecf46927d2

--- src/connectivity/bluetooth/core/bt-host/gap/pairing_state.h
+++ src/connectivity/bluetooth/core/bt-host/gap/pairing_state.h
@@ -7,6 +7,8 @@
 
 #include <fbl/macros.h>
 
+#include <optional>
+
 #include "src/connectivity/bluetooth/core/bt-host/hci/hci.h"
 #include "src/connectivity/bluetooth/core/bt-host/sm/smp.h"
 
@@ -35,14 +37,20 @@ enum class PairingAction {
   kRequestPasskey,
 };
 
+// This class is not thread-safe and should only be called on the thread on
+// which it was created.
 class PairingState final {
  public:
-  // Used to report the status of a pairing procedure.
+  // Used to report the status of a pairing procedure. |status| will contain
+  // HostError::kNotSupported if the pairing procedure does not proceed in the
+  // order of events expected.
   using StatusCallback =
       fit::function<void(hci::ConnectionHandle, hci::Status)>;
   PairingState(StatusCallback status_cb);
   ~PairingState() = default;
 
+  // True if there is currently a pairing procedure in progress that the local
+  // device initiated.
   bool initiator() const { return initiator_; }
 
   // Starts pairing against the peer, if pairing is not already in progress.
@@ -55,7 +63,45 @@ class PairingState final {
   };
   [[nodiscard]] InitiatorAction InitiatePairing();
 
+  // Event handlers. Caller must ensure that the event is addressed to the link
+  // for this PairingState.
+
+  // Returns value for IO Capability Request Reply, else std::nullopt for IO
+  // Capability Negative Reply.
+  //
+  // TODO(BT-8): Indicate presence of out-of-band (OOB) data.
+  [[nodiscard]] std::optional<hci::IOCapability> OnIoCapabilityRequest();
+
+  // Caller is not expected to send a response.
   void OnIoCapabilityResponse(hci::IOCapability peer_iocap);
+
+  // |cb| is called with: true to send User Confirmation Request Reply, else
+  // for to send User Confirmation Request Negative Reply.
+  using UserConfirmationCallback = fit::callback<void(bool confirm)>;
+  void OnUserConfirmationRequest(uint32_t numeric_value,
+                                 UserConfirmationCallback cb);
+
+  // |cb| is called with: passkey value to send User Passkey Request Reply, else
+  // std::nullopt to send User Passkey Request Negative Reply.
+  using UserPasskeyCallback =
+      fit::callback<void(std::optional<uint32_t> passkey)>;
+  void OnUserPasskeyRequest(UserPasskeyCallback cb);
+
+  // Caller is not expected to send a response.
+  void OnUserPasskeyNotification(uint32_t numeric_value);
+
+  // Caller is not expected to send a response.
+  void OnSimplePairingComplete(hci::StatusCode status_code);
+
+  // Caller is not expected to send a response.
+  void OnLinkKeyNotification(const UInt128& link_key,
+                             hci::LinkKeyType key_type);
+
+  // Caller is not expected to send a response.
+  void OnAuthenticationComplete(hci::StatusCode status_code);
+
+  // Handler for hci::Connection::set_encryption_change_callback.
+  void OnEncryptionChange(hci::Status status, bool enabled);
 
  private:
   enum class State {
@@ -91,10 +137,27 @@ class PairingState final {
     kFailed,
   };
 
+  static const char* ToString(State state);
+
   State state() const { return state_; }
 
+  // Called to enable encryption on the link for this peer. Sets |state_| to
+  // kWaitEncryption.
+  void EnableEncryption();
+
+  // Called when an event is received while in a state that doesn't expect that
+  // event. Invokes |status_callback_| with HostError::kNotSupported and sets
+  // |state_| to kFailed.
+  void FailWithUnexpectedEvent();
+
+  // Set to true by locally-initiated pairing and cleared when pairing is reset.
   bool initiator_;
+
+  // State machine representation.
   State state_;
+
+  // Holds the callback that this object was constructed with.
+  StatusCallback status_callback_;
 
   DISALLOW_COPY_AND_ASSIGN_ALLOW_MOVE(PairingState);
 };
