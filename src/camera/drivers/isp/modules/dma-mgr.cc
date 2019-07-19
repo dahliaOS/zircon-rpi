83e093552d562bf62330b1a74d2768712835b405
Garratt <garratt@google.com>
2019-07-19 02:08:48 -0700

    [ISP][Dma] Added DmaMgr Enable/Disable
    Theis CL makes the Dma manager usage more
    straightforward, and better aligned with other
    camera components, in that it has 3 main functions:
    Configure - Set up / Connect to stream
    Enable    - Start streaming frames
    Disable   - Stop streaming frames
    I didn't use the Connect/Start/Stop function
    names because the dma manager does not actually start and stop
    the stream, but rather it enables frames to be written
    when they arrive.
    
    Testability:  I updated the unit test. For more info on
    how to test, read src/camera/TESTING.md
    
    Change-Id: I9c0b8fc85e677ceb6b0c18b7c8666e9e22976c02

--- src/camera/drivers/isp/modules/dma-mgr.cc
+++ src/camera/drivers/isp/modules/dma-mgr.cc
@@ -134,7 +134,7 @@ void DmaManager::PrintStatus(ddk::MmioBuffer *mmio) {
 }
 
 
-zx_status_t DmaManager::Start(
+zx_status_t DmaManager::Configure(
     fuchsia_sysmem_BufferCollectionInfo buffer_collection,
     fit::function<void(fuchsia_camera_common_FrameAvailableEvent)>
         frame_available_callback) {
@@ -175,10 +175,20 @@ zx_status_t DmaManager::Start(
     return status;
   }
   frame_available_callback_ = std::move(frame_available_callback);
-  enabled_ = true;
   return ZX_OK;
 }
 
+void DmaManager::Enable() {
+  ZX_ASSERT(frame_available_callback_ != nullptr);
+  enabled_ = true;
+}
+
+void DmaManager::Disable() {
+  enabled_ = false;
+  // TODO(CAM-54): Provide a way to dump the previous set of write locked
+  // buffers.
+  write_locked_buffers_.clear();
+}
 
 void DmaManager::OnFrameWritten() {
   // If we have not started streaming, just skip.
