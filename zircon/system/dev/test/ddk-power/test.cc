ba00a1511802d05c2f1ee85a3e64012108ce1954
Ruchira Ravoori <ravoorir@google.com>
2019-08-24 21:03:36 -0700

    test changes
    Change-Id: I4ad75563847d8592ef9d00815a670d3bf995e163

--- zircon/system/dev/test/ddk-power/test.cc
+++ zircon/system/dev/test/ddk-power/test.cc
@@ -49,7 +49,28 @@ class PowerTestCase : public zxtest::Test {
         fdio_get_service_handle(child_fd.release(), child_device_handle.reset_and_get_address()));
     ASSERT_NE(child_device_handle.get(), ZX_HANDLE_INVALID);
   }
+  void AddChildWithPowerArgs(DevicePowerStateInfo* states, uint8_t count) {
+    auto power_states =
+        ::fidl::VectorView<DevicePowerStateInfo>(count, reinterpret_cast<DevicePowerStateInfo *>(states));
+    auto response = TestDevice::Call::AddDeviceWithPowerArgs(zx::unowned(child_device_handle),
+                                                             power_states);
+    ASSERT_OK(response.status());
+    zx_status_t call_status = ZX_OK;
+    if (response->result.is_err()) {
+      call_status = response->result.err();
+    }
+    ASSERT_OK(call_status);
+
+    fbl::unique_fd child2_fd;
+    ASSERT_OK(devmgr_integration_test::RecursiveWaitForFile(
+        devmgr.devfs_root(), "sys/platform/11:0b:0/power-test/power-test-child-2", &child2_fd));
+    ASSERT_GT(child2_fd.get(), 0);
+    ASSERT_OK(
+        fdio_get_service_handle(child2_fd.release(), child2_device_handle.reset_and_get_address()));
+    ASSERT_NE(child2_device_handle.get(), ZX_HANDLE_INVALID);
+  }
   zx::channel child_device_handle;
+  zx::channel child2_device_handle;
   IsolatedDevmgr devmgr;
 };
 
@@ -160,29 +181,12 @@ TEST_F(PowerTestCase, GetDevicePowerCaps_Success) {
   states[2].state_id = DevicePowerState::DEVICE_POWER_STATE_D3COLD;
   states[2].is_supported = true;
   states[2].restore_latency = 1000;
-  auto power_states =
-      ::fidl::VectorView<DevicePowerStateInfo>(3, reinterpret_cast<DevicePowerStateInfo *>(states));
-  auto response = TestDevice::Call::AddDeviceWithPowerArgs(zx::unowned(child_device_handle),
-                                                           power_states);
-  ASSERT_OK(response.status());
-  zx_status_t call_status = ZX_OK;
-  if (response->result.is_err()) {
-    call_status = response->result.err();
-  }
-  ASSERT_OK(call_status);
-
-  fbl::unique_fd child2_fd;
-  ASSERT_OK(devmgr_integration_test::RecursiveWaitForFile(
-      devmgr.devfs_root(), "sys/platform/11:0b:0/power-test/power-test-child-2", &child2_fd));
-  ASSERT_GT(child2_fd.get(), 0);
-  zx::channel child2_device_handle;
-  ASSERT_OK(
-      fdio_get_service_handle(child2_fd.release(), child2_device_handle.reset_and_get_address()));
-  ASSERT_NE(child2_device_handle.get(), ZX_HANDLE_INVALID);
+  AddChildWithPowerArgs(states, 3);
 
   const DevicePowerStateInfo *out_dpstates;
   auto response2 = Controller::Call::GetDevicePowerCaps(zx::unowned(child2_device_handle));
   ASSERT_OK(response2.status());
+  zx_status_t call_status;
   if (response2->result.is_err()) {
     call_status = response2->result.err();
   }
@@ -215,25 +219,8 @@ TEST_F(PowerTestCase, Suspend_Success) {
   states[2].state_id = DevicePowerState::DEVICE_POWER_STATE_D3COLD;
   states[2].is_supported = true;
   states[2].restore_latency = 1000;
-  auto power_states =
-      ::fidl::VectorView<DevicePowerStateInfo>(3, reinterpret_cast<DevicePowerStateInfo *>(states));
-  auto response = TestDevice::Call::AddDeviceWithPowerArgs(zx::unowned(child_device_handle),
-                                                           power_states);
-  ASSERT_OK(response.status());
-  zx_status_t call_status = ZX_OK;
-  if (response->result.is_err()) {
-    call_status = response->result.err();
-  }
-  ASSERT_OK(call_status);
-
-  fbl::unique_fd child2_fd;
-  ASSERT_OK(devmgr_integration_test::RecursiveWaitForFile(
-      devmgr.devfs_root(), "sys/platform/11:0b:0/power-test/power-test-child-2", &child2_fd));
-  ASSERT_GT(child2_fd.get(), 0);
-  zx::channel child2_device_handle;
-  ASSERT_OK(
-      fdio_get_service_handle(child2_fd.release(), child2_device_handle.reset_and_get_address()));
-  ASSERT_NE(child2_device_handle.get(), ZX_HANDLE_INVALID);
+  AddChildWithPowerArgs(states, 3);
+  zx_status_t call_status;
 
   auto suspend_result = Controller::Call::Suspend(zx::unowned(child2_device_handle),
                                                   DevicePowerState::DEVICE_POWER_STATE_D3COLD);
@@ -265,25 +252,8 @@ TEST_F(PowerTestCase, Resume_Success) {
   states[2].state_id = DevicePowerState::DEVICE_POWER_STATE_D3COLD;
   states[2].is_supported = true;
   states[2].restore_latency = 1000;
-  auto power_states =
-      ::fidl::VectorView<DevicePowerStateInfo>(3, reinterpret_cast<DevicePowerStateInfo *>(states));
-  auto response = TestDevice::Call::AddDeviceWithPowerArgs(zx::unowned(child_device_handle),
-                                                           power_states);
-  ASSERT_OK(response.status());
-  zx_status_t call_status = ZX_OK;
-  if (response->result.is_err()) {
-    call_status = response->result.err();
-  }
-  ASSERT_OK(call_status);
-
-  fbl::unique_fd child2_fd;
-  ASSERT_OK(devmgr_integration_test::RecursiveWaitForFile(
-      devmgr.devfs_root(), "sys/platform/11:0b:0/power-test/power-test-child-2", &child2_fd));
-  ASSERT_GT(child2_fd.get(), 0);
-  zx::channel child2_device_handle;
-  ASSERT_OK(
-      fdio_get_service_handle(child2_fd.release(), child2_device_handle.reset_and_get_address()));
-  ASSERT_NE(child2_device_handle.get(), ZX_HANDLE_INVALID);
+  AddChildWithPowerArgs(states, 3);
+  zx_status_t call_status;
 
   auto suspend_result = Controller::Call::Suspend(zx::unowned(child2_device_handle),
                                                   DevicePowerState::DEVICE_POWER_STATE_D3COLD);
