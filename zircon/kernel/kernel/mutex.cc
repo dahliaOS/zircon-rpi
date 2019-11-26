3c0ef82372f48e9590948466982b423e20d203fd
Corey Tabaka <eieio@google.com>
2019-11-26 01:11:19 -0800

    wip - disable mutex tracing
    Change-Id: Ib5cc653e26cbf18770c26ecbd3e9a21159d66ea6

--- zircon/kernel/kernel/mutex.cc
+++ zircon/kernel/kernel/mutex.cc
@@ -44,7 +44,7 @@ enum class KernelMutexTracingLevel {
 };
 
 // By default, kernel mutex tracing is disabled.
-template <KernelMutexTracingLevel = KernelMutexTracingLevel::All, typename = void>
+template <KernelMutexTracingLevel = KernelMutexTracingLevel::None, typename = void>
 class KTracer;
 
 template <>
